f79cfd1d6af3f849745413ed26d8299a55ff4b1b
Banyc <36535895+Banyc@users.noreply.github.com>
2023-10-21 19:44:20 +0800

    feat: recv_buf

---
+++ src/message.rs
@@ -0,0 +1,66 @@
+use bytes::{Buf, BytesMut};
+
+#[derive(Debug)]
+pub struct DataSegment {
+    start_sequence: Sequence,
+    payload: BytesMut,
+}
+
+impl DataSegment {
+    pub fn new(start_sequence: Sequence, payload: BytesMut) -> Self {
+        Self {
+            start_sequence,
+            payload,
+        }
+    }
+
+    pub fn start_sequence(&self) -> Sequence {
+        self.start_sequence
+    }
+
+    pub fn end_sequence(&self) -> Sequence {
+        Sequence::new(self.start_sequence.inner() + self.payload.len() as u64)
+    }
+
+    pub fn advance(mut self, bytes: usize) -> Option<Self> {
+        if self.payload.len() <= bytes {
+            return None;
+        }
+
+        self.start_sequence = Sequence::new(self.start_sequence.inner() + bytes as u64);
+        self.payload.advance(bytes);
+
+        Some(self)
+    }
+
+    pub fn advance_to(self, end: Sequence) -> Option<Self> {
+        let bytes = end.inner().saturating_sub(self.start_sequence.inner());
+        let bytes = match usize::try_from(bytes) {
+            Ok(bytes) => bytes,
+            Err(_) => return None,
+        };
+
+        self.advance(bytes)
+    }
+
+    pub fn len(&self) -> usize {
+        self.payload.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.payload.is_empty()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, std::hash::Hash)]
+pub struct Sequence(u64);
+
+impl Sequence {
+    pub fn new(number: u64) -> Self {
+        Self(number)
+    }
+
+    pub fn inner(&self) -> u64 {
+        self.0
+    }
+}
---
+++ src/recv_buf.rs
@@ -0,0 +1,150 @@
+use std::collections::BTreeMap;
+
+use crate::message::{DataSegment, Sequence};
+
+pub struct RecvStreamBuf {
+    next: Sequence,
+    data_segments: BTreeMap<Sequence, DataSegment>,
+}
+
+impl RecvStreamBuf {
+    pub fn new() -> Self {
+        Self {
+            next: Sequence::new(0),
+            data_segments: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, data_segment: DataSegment) {
+        // Remove stale data
+        let data_segment = match data_segment.advance_to(self.next) {
+            Some(data_segment) => data_segment,
+            None => return,
+        };
+
+        // Resolve key conflict
+        if let Some(old_data_segment) = self.data_segments.get(&data_segment.start_sequence()) {
+            if data_segment.len() <= old_data_segment.len() {
+                return;
+            }
+        }
+
+        self.data_segments
+            .insert(data_segment.start_sequence(), data_segment);
+    }
+
+    pub fn pop_first(&mut self) -> Option<DataSegment> {
+        let (first_key, _) = match self.data_segments.first_key_value() {
+            Some(first) => first,
+            None => return None,
+        };
+        if *first_key != self.next {
+            return None;
+        }
+
+        let (_, first_segment) = self.data_segments.pop_first().unwrap();
+
+        self.next = first_segment.end_sequence();
+
+        // Deduplicate data
+        loop {
+            if let Some((start_sequence, _)) = self.data_segments.first_key_value() {
+                if self.next <= *start_sequence {
+                    break;
+                }
+            }
+            let data_segment = match self.data_segments.pop_first() {
+                Some((_, v)) => v,
+                None => break,
+            };
+
+            let data_segment = data_segment.advance_to(self.next);
+            if let Some(data_segment) = data_segment {
+                self.data_segments
+                    .insert(data_segment.start_sequence(), data_segment);
+            }
+        }
+
+        Some(first_segment)
+    }
+}
+
+impl Default for RecvStreamBuf {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bytes::BytesMut;
+
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let mut buf = RecvStreamBuf::new();
+        buf.insert(DataSegment::new(
+            Sequence::new(0),
+            BytesMut::from_iter(vec![0, 1, 2]),
+        ));
+        let data_segment = buf.pop_first().unwrap();
+        assert_eq!(buf.next, data_segment.end_sequence());
+        assert!(buf.pop_first().is_none());
+    }
+
+    #[test]
+    fn remove_stale_data() {
+        let mut buf = RecvStreamBuf::new();
+        buf.insert(DataSegment::new(
+            Sequence::new(0),
+            BytesMut::from_iter(vec![0, 1, 2]),
+        ));
+        let _ = buf.pop_first().unwrap();
+        buf.insert(DataSegment::new(
+            Sequence::new(0),
+            BytesMut::from_iter(vec![0, 1, 2, 3, 4]),
+        ));
+        let data_segment = buf.pop_first().unwrap();
+        assert_eq!(data_segment.start_sequence(), Sequence::new(3));
+        assert_eq!(data_segment.len(), 2);
+        assert_eq!(buf.next, data_segment.end_sequence());
+        assert!(buf.pop_first().is_none());
+    }
+
+    #[test]
+    fn deduplicate_1() {
+        let mut buf = RecvStreamBuf::new();
+        buf.insert(DataSegment::new(
+            Sequence::new(0),
+            BytesMut::from_iter(vec![0, 1, 2]),
+        ));
+        buf.insert(DataSegment::new(
+            Sequence::new(1),
+            BytesMut::from_iter(vec![1, 2]),
+        ));
+        let _ = buf.pop_first().unwrap();
+        assert_eq!(buf.next, Sequence::new(3));
+        assert!(buf.pop_first().is_none());
+    }
+
+    #[test]
+    fn deduplicate_2() {
+        let mut buf = RecvStreamBuf::new();
+        buf.insert(DataSegment::new(
+            Sequence::new(0),
+            BytesMut::from_iter(vec![0, 1, 2]),
+        ));
+        buf.insert(DataSegment::new(
+            Sequence::new(1),
+            BytesMut::from_iter(vec![1, 2, 3]),
+        ));
+        let _ = buf.pop_first().unwrap();
+        assert_eq!(buf.next, Sequence::new(3));
+        let data_segment = buf.pop_first().unwrap();
+        assert_eq!(data_segment.len(), 1);
+        assert_eq!(data_segment.start_sequence(), Sequence::new(3));
+        assert_eq!(buf.next, Sequence::new(4));
+        assert!(buf.pop_first().is_none());
+    }
+}
