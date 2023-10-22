c3b9318c46af68fa7794b5b76c187b2184bdb325
Banyc <36535895+Banyc@users.noreply.github.com>
2023-10-23 02:39:13 +0800

    feat: protect listener

--- src/listen.rs
+++ src/listen.rs
@@ -1,23 +1,53 @@
-use std::{collections::HashMap, io, num::NonZeroUsize};
+use std::{
+    collections::HashMap,
+    io,
+    num::NonZeroUsize,
+    sync::{Arc, RwLock},
+    time::{Duration, Instant},
+};
 
-use tokio::net::{tcp, TcpListener, TcpStream, ToSocketAddrs};
+use tokio::{
+    net::{tcp, TcpListener, TcpStream, ToSocketAddrs},
+    task::JoinSet,
+};
 
 use crate::{
     message::{Init, Session},
     stream::MptcpStream,
 };
 
+const BACKLOG_TIMEOUT: Duration = Duration::from_secs(60);
+const BACKLOG_MAX: usize = 1024;
+
 pub struct MptcpListener {
     listener: TcpListener,
-    backlog: HashMap<Session, Backlog>,
+    backlog: Arc<RwLock<HashMap<Session, QueuedConnection>>>,
+    backlog_max: NonZeroUsize,
+    _backlog_cleanup_task: JoinSet<()>,
 }
 
 impl MptcpListener {
     pub async fn bind(addr: impl ToSocketAddrs) -> io::Result<Self> {
         let listener = TcpListener::bind(addr).await?;
+        let backlog = Arc::new(RwLock::new(HashMap::new()));
+        let mut backlog_cleanup_task = JoinSet::new();
+        backlog_cleanup_task.spawn({
+            let backlog = Arc::clone(&backlog);
+            async move {
+                loop {
+                    tokio::time::sleep(BACKLOG_TIMEOUT / 2).await;
+
+                    let mut backlog = backlog.write().unwrap();
+                    backlog.retain(|_, v: &mut QueuedConnection| !v.timed_out(BACKLOG_TIMEOUT));
+                }
+            }
+        });
+
         Ok(Self {
             listener,
-            backlog: HashMap::new(),
+            backlog,
+            backlog_max: NonZeroUsize::new(BACKLOG_MAX).unwrap(),
+            _backlog_cleanup_task: backlog_cleanup_task,
         })
     }
 
@@ -26,20 +56,24 @@ impl MptcpListener {
             let (mut stream, _) = self.listener.accept().await?;
             let init = Init::decode(&mut stream).await?;
             loop {
-                match self.backlog.remove(&init.session()) {
-                    Some(backlog) => {
-                        let res = backlog.push(stream);
+                let mut backlog = self.backlog.write().unwrap();
+                match backlog.remove(&init.session()) {
+                    Some(queued_connection) => {
+                        let res = queued_connection.push(stream);
                         match res {
-                            BacklogPushResult::Backlog(backlog) => {
-                                self.backlog.insert(init.session(), backlog);
+                            QueuedConnectionPushResult::QueuedConnection(queued_connection) => {
+                                backlog.insert(init.session(), queued_connection);
                                 break;
                             }
-                            BacklogPushResult::Stream(stream) => return Ok(stream),
+                            QueuedConnectionPushResult::Stream(stream) => return Ok(stream),
                         }
                     }
                     None => {
-                        let backlog = Backlog::new(init.streams());
-                        self.backlog.insert(init.session(), backlog);
+                        if backlog.len() >= self.backlog_max.get() {
+                            break;
+                        }
+                        let queued_connection = QueuedConnection::new(init.streams());
+                        backlog.insert(init.session(), queued_connection);
                     }
                 };
             }
@@ -47,34 +81,41 @@ impl MptcpListener {
     }
 }
 
-struct Backlog {
+struct QueuedConnection {
     read_streams: Vec<tcp::OwnedReadHalf>,
     write_streams: Vec<tcp::OwnedWriteHalf>,
     max: NonZeroUsize,
+    last_update: Instant,
 }
 
-impl Backlog {
+impl QueuedConnection {
     pub fn new(number: NonZeroUsize) -> Self {
         Self {
             read_streams: Vec::new(),
             write_streams: Vec::new(),
             max: number,
+            last_update: Instant::now(),
         }
     }
 
-    pub fn push(mut self, stream: TcpStream) -> BacklogPushResult {
+    pub fn push(mut self, stream: TcpStream) -> QueuedConnectionPushResult {
         let (read, write) = stream.into_split();
         self.read_streams.push(read);
         self.write_streams.push(write);
         if self.read_streams.len() == self.max.get() {
             let stream = MptcpStream::new(self.read_streams, self.write_streams);
-            return BacklogPushResult::Stream(stream);
+            return QueuedConnectionPushResult::Stream(stream);
         }
-        BacklogPushResult::Backlog(self)
+        self.last_update = Instant::now();
+        QueuedConnectionPushResult::QueuedConnection(self)
+    }
+
+    pub fn timed_out(&self, timeout: Duration) -> bool {
+        self.last_update.elapsed() > timeout
     }
 }
 
-enum BacklogPushResult {
-    Backlog(Backlog),
+enum QueuedConnectionPushResult {
+    QueuedConnection(QueuedConnection),
     Stream(MptcpStream),
 }
