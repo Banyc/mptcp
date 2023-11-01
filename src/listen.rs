9dc6297fb276eace16d2207bc44d08445e1828b3
Banyc <36535895+Banyc@users.noreply.github.com>
2023-11-02 02:14:09 +0800

    feat: parallel TCP accepts

--- src/listen.rs
+++ src/listen.rs
@@ -23,11 +23,9 @@ const BACKLOG_MAX: usize = 1024;
 
 #[derive(Debug)]
 pub struct MptcpListener {
-    listener: TcpListener,
-    backlog: Arc<RwLock<HashMap<Session, QueuedConnection>>>,
-    backlog_max: NonZeroUsize,
-    max_session_streams: NonZeroUsize,
-    _backlog_cleanup_task: JoinSet<()>,
+    listener: Arc<TcpListener>,
+    complete: tokio::sync::mpsc::Receiver<io::Result<MptcpStream>>,
+    _tasks: JoinSet<()>,
 }
 
 impl MptcpListener {
@@ -35,64 +33,131 @@ impl MptcpListener {
         addr: impl ToSocketAddrs,
         max_session_streams: NonZeroUsize,
     ) -> io::Result<Self> {
-        let listener = TcpListener::bind(addr).await?;
-        let backlog = Arc::new(RwLock::new(HashMap::new()));
-        let mut backlog_cleanup_task = JoinSet::new();
-        backlog_cleanup_task.spawn({
+        let listener = Arc::new(TcpListener::bind(addr).await?);
+        let (tx, rx) = tokio::sync::mpsc::channel(BACKLOG_MAX);
+
+        let backlog = Arc::new(Backlog::new(
+            NonZeroUsize::new(BACKLOG_MAX).unwrap(),
+            max_session_streams,
+        ));
+
+        let mut tasks = JoinSet::new();
+        for _ in 0..BACKLOG_MAX {
+            let backlog = Arc::clone(&backlog);
+            let listener = Arc::clone(&listener);
+            let complete = tx.clone();
+            tasks.spawn(async move {
+                loop {
+                    let (stream, _) = match listener.accept().await {
+                        Ok(x) => x,
+                        Err(e) => {
+                            let _ = complete.send(Err(e)).await;
+                            continue;
+                        }
+                    };
+                    backlog.handle(stream, &complete).await;
+                }
+            });
+        }
+        tasks.spawn({
             let backlog = Arc::clone(&backlog);
             async move {
                 loop {
                     tokio::time::sleep(BACKLOG_TIMEOUT / 2).await;
 
-                    let mut backlog = backlog.write().unwrap();
-                    backlog.retain(|_, v: &mut QueuedConnection| !v.timed_out(BACKLOG_TIMEOUT));
+                    backlog.clean(BACKLOG_TIMEOUT);
                 }
             }
         });
 
         Ok(Self {
             listener,
-            backlog,
-            backlog_max: NonZeroUsize::new(BACKLOG_MAX).unwrap(),
-            max_session_streams,
-            _backlog_cleanup_task: backlog_cleanup_task,
+            complete: rx,
+            _tasks: tasks,
         })
     }
 
-    pub async fn accept(&self) -> io::Result<MptcpStream> {
-        loop {
-            let (mut stream, _) = self.listener.accept().await?;
-            let init = tokio::time::timeout(INIT_TIMEOUT, Init::decode(&mut stream)).await??;
-            if init.streams() > self.max_session_streams {
-                continue;
-            }
-            loop {
-                let mut backlog = self.backlog.write().unwrap();
-                match backlog.remove(&init.session()) {
-                    Some(queued_connection) => {
-                        let res = queued_connection.push(stream);
-                        match res {
-                            QueuedConnectionPushResult::QueuedConnection(queued_connection) => {
-                                backlog.insert(init.session(), queued_connection);
-                                break;
-                            }
-                            QueuedConnectionPushResult::Stream(stream) => return Ok(stream),
-                        }
-                    }
-                    None => {
-                        if backlog.len() >= self.backlog_max.get() {
-                            break;
-                        }
-                        let queued_connection = QueuedConnection::new(init.streams());
-                        backlog.insert(init.session(), queued_connection);
-                    }
-                };
-            }
-        }
+    /// # Cancel safety
+    ///
+    /// This method is cancel safe. If `accept` is used as the event in a `tokio::select` statement and some other branch completes first, it is guaranteed that no streams were received on this listener.
+    pub async fn accept(&mut self) -> io::Result<MptcpStream> {
+        self.complete
+            .recv()
+            .await
+            .expect("senders will never drop proactively")
     }
 
     pub fn local_addr(&self) -> io::Result<SocketAddr> {
         self.listener.local_addr()
+    }
+}
+
+#[derive(Debug)]
+struct Backlog {
+    queued: RwLock<HashMap<Session, QueuedConnection>>,
+    queue_max: NonZeroUsize,
+    max_session_streams: NonZeroUsize,
+}
+
+impl Backlog {
+    pub fn new(queue_max: NonZeroUsize, max_session_streams: NonZeroUsize) -> Self {
+        Self {
+            queued: RwLock::new(HashMap::new()),
+            queue_max,
+            max_session_streams,
+        }
+    }
+
+    pub fn clean(&self, timeout: Duration) {
+        self.queued
+            .write()
+            .unwrap()
+            .retain(|_, v: &mut QueuedConnection| !v.timed_out(timeout));
+    }
+
+    pub async fn handle(
+        &self,
+        stream: TcpStream,
+        complete: &tokio::sync::mpsc::Sender<io::Result<MptcpStream>>,
+    ) {
+        async fn handle_impl(
+            this: &Backlog,
+            mut stream: TcpStream,
+            complete: &tokio::sync::mpsc::Sender<io::Result<MptcpStream>>,
+        ) -> io::Result<()> {
+            let init = tokio::time::timeout(INIT_TIMEOUT, Init::decode(&mut stream)).await??;
+            if init.streams() > this.max_session_streams {
+                return Ok(());
+            }
+
+            let stream = loop {
+                let mut queued = this.queued.write().unwrap();
+                match queued.remove(&init.session()) {
+                    Some(queued_connection) => {
+                        let res = queued_connection.push(stream);
+                        match res {
+                            QueuedConnectionPushResult::QueuedConnection(queued_connection) => {
+                                queued.insert(init.session(), queued_connection);
+                                break None;
+                            }
+                            QueuedConnectionPushResult::Stream(stream) => break Some(stream),
+                        }
+                    }
+                    None => {
+                        if queued.len() >= this.queue_max.get() {
+                            break None;
+                        }
+                        let queued_connection = QueuedConnection::new(init.streams());
+                        queued.insert(init.session(), queued_connection);
+                    }
+                }
+            };
+            if let Some(stream) = stream {
+                let _ = complete.send(Ok(stream)).await;
+            }
+            Ok(())
+        }
+        let _ = handle_impl(self, stream, complete).await;
     }
 }
 
